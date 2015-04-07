4151e76e833fb48e48e777bf13d0ef17d4707af1
Richard Brush <brushric@msu.edu>
2015-04-07 19:29:04 -0400

    Logging in and adding a new user.

--- app/src/main/java/edu/msu/brushric/theflockinggame/Cloud.java
+++ app/src/main/java/edu/msu/brushric/theflockinggame/Cloud.java
@@ -34,179 +34,7 @@ public class Cloud {
     private static final String LOGIN_URL = "http://webdev.cse.msu.edu/~sarteleb/cse476/toucan/toucan-login.php";
     private static final String NEW_USER_URL = "http://webdev.cse.msu.edu/~sarteleb/cse476/toucan/toucan-new-user.php";
     private static final String UTF8 = "UTF-8";
-//
-//    /**
-//     * Nested class to store one catalog row
-//     */
-//    private static class Item {
-//        public String name = "";
-//        public String id = "";
-//    }
-//
-//    /**
-//     * An adapter so that list boxes can display a list of filenames from
-//     * the cloud server.
-//     */
-//    public static class CatalogAdapter extends BaseAdapter {
-//        /**
-//         * Constructor
-//         */
-//        public CatalogAdapter(final View view) {
-//            // Create a thread to load the catalog
-//            new Thread(new Runnable() {
-//
-//                @Override
-//                public void run() {
-//                    ArrayList<Item> newItems = getCatalog();
-//                    if(newItems != null) {
-//
-//                        items = newItems;
-//
-//                        view.post(new Runnable() {
-//
-//                            @Override
-//                            public void run() {
-//                                // Tell the adapter the data set has been changed
-//                                notifyDataSetChanged();
-//                            }
-//
-//                        });
-//                    } else {
-//                        // Error condition!
-//                        view.post(new Runnable() {
-//
-//                            @Override
-//                            public void run() {
-//                                //Toast.makeText(view.getContext(), R.string.catalog_fail, Toast.LENGTH_SHORT).show();
-//                            }
-//
-//                        });
-//                    }
-//
-//                }
-//
-//            }).start();
-//        }
-//
-//        /**
-//         * Get the catalog items from the server
-//         * @return Array of items or null if failed
-//         */
-//        public ArrayList<Item> getCatalog() {
-//            ArrayList<Item> newItems = new ArrayList<Item>();
-//
-//            // Create a GET query
-//            String query = CATALOG_URL + "?user=" + USER + "&magic=" + MAGIC + "&pw=" + PASSWORD;
-//
-//            /**
-//             * Open the connection
-//             */
-//            InputStream stream = null;
-//            try {
-//                URL url = new URL(query);
-//
-//                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-//                int responseCode = conn.getResponseCode();
-//                if(responseCode != HttpURLConnection.HTTP_OK) {
-//                    return null;
-//                }
-//
-//                stream = conn.getInputStream();
-//
-//                //For debugging
-//                //logStream(stream);
-//
-//                /**
-//                 * Create an XML parser for the result
-//                 */
-//                try {
-//                    XmlPullParser xml = Xml.newPullParser();
-//                    xml.setInput(stream, "UTF-8");
-//
-//                    xml.nextTag();      // Advance to first tag
-//                    xml.require(XmlPullParser.START_TAG, null, "hatter");
-//
-//                    String status = xml.getAttributeValue(null, "status");
-//                    if(status.equals("no")) {
-//                        return null;
-//                    }
-//
-//                    while(xml.nextTag() == XmlPullParser.START_TAG) {
-//                        if(xml.getName().equals("hatting")) {
-//                            Item item = new Item();
-//                            item.name = xml.getAttributeValue(null, "name");
-//                            item.id = xml.getAttributeValue(null, "id");
-//                            newItems.add(item);
-//                        }
-//
-//                        skipToEndTag(xml);
-//                    }
-//
-//                    // We are done
-//                } catch(XmlPullParserException ex) {
-//                    return null;
-//                } catch(IOException ex) {
-//                    return null;
-//                } finally {
-//                    try {
-//                        stream.close();
-//                    } catch(IOException ex) {
-//
-//                    }
-//                }
-//
-//            } catch (MalformedURLException e) {
-//                // Should never happen
-//                return null;
-//            } catch (IOException ex) {
-//                return null;
-//            }
-//
-//            return newItems;
-//        }
-//
-//        /**
-//         * The items we display in the list box. Initially this is
-//         * null until we get items from the server.
-//         */
-//        private ArrayList<Item> items = new ArrayList<Item>();
-//
-//        @Override
-//        public int getCount() {
-//            return items.size();
-//        }
-//
-//        @Override
-//        public Item getItem(int position) {
-//            return items.get(position);
-//        }
-//
-//        @Override
-//        public long getItemId(int position) {
-//            return position;
-//        }
-//
-//        @Override
-//        public View getView(int position, View view, ViewGroup parent) {
-//            if(view == null) {
-//                view = LayoutInflater.from(parent.getContext()).inflate(R.layout.catalog_item, parent, false);
-//            }
-//
-//            TextView tv = (TextView)view.findViewById(R.id.textItem);
-//            tv.setText(items.get(position).name);
-//
-//            return view;
-//        }
-//
-//        public String getId(int position) {
-//            return items.get(position).id;
-//        }
-//
-//        public String getName(int position) {
-//            return items.get(position).name;
-//        }
-//    }
-//
+
     /**
      * Skip the XML parser to the end tag for whatever
      * tag we are currently within.
@@ -245,7 +73,30 @@ public class Cloud {
                 return false;
             }
 
-            //InputStream stream = conn.getInputStream();
+            InputStream stream = conn.getInputStream();
+
+            /**
+             * Create an XML parser for the result
+             */
+            try {
+                XmlPullParser xmlR = Xml.newPullParser();
+                xmlR.setInput(stream, UTF8);
+
+                xmlR.nextTag();      // Advance to first tag
+                xmlR.require(XmlPullParser.START_TAG, null, "toucan");
+
+                String status = xmlR.getAttributeValue(null, "status");
+                if(status.equals("no")) {
+                    return false;
+                }
+
+                // We are done
+            } catch(XmlPullParserException ex) {
+                return false;
+            } catch(IOException ex) {
+                return false;
+            }
+            stream.close();
             return true;
 
         } catch (MalformedURLException e) {
@@ -273,8 +124,31 @@ public class Cloud {
                 return false;
             }
 
-            //InputStream stream = conn.getInputStream();
-            return true;
+            InputStream stream = conn.getInputStream();
+
+            /**
+             * Create an XML parser for the result
+             */
+            try {
+                XmlPullParser xmlR = Xml.newPullParser();
+                xmlR.setInput(stream, UTF8);
+
+                xmlR.nextTag();      // Advance to first tag
+                xmlR.require(XmlPullParser.START_TAG, null, "toucan");
+
+                String status = xmlR.getAttributeValue(null, "status");
+                if(status.equals("no")) {
+                    return false;
+                }
+
+                // We are done
+            } catch(XmlPullParserException ex) {
+                return false;
+            } catch(IOException ex) {
+                return false;
+            }
+            stream.close();
+            return loginUser(user, password);
 
         } catch (MalformedURLException e) {
             // Should never happen
@@ -285,189 +159,4 @@ public class Cloud {
 
 
     }
-//    /**
-//     * Save a hatting to the cloud.
-//     * This should be run in a thread.
-//     * @param name name to save under
-//     * @param view view we are getting the data from
-//     * @return true if successful
-//     */
-//    public boolean saveToCloud(String name, HatterView view) {
-//        name = name.trim();
-//        if(name.length() == 0) {
-//            return false;
-//        }
-//
-//        // Create an XML packet with the information about the current image
-//        XmlSerializer xml = Xml.newSerializer();
-//        StringWriter writer = new StringWriter();
-//
-//        try {
-//            xml.setOutput(writer);
-//
-//            xml.startDocument("UTF-8", true);
-//
-//            xml.startTag(null, "hatter");
-//            xml.attribute(null, "user", USER);
-//            xml.attribute(null, "pw", PASSWORD);
-//            xml.attribute(null, "magic", MAGIC);
-//
-//            view.saveXml(name, xml);
-//
-//            xml.endTag(null, "hatter");
-//
-//            xml.endDocument();
-//
-//        } catch (IOException e) {
-//            // This won't occur when writing to a string
-//            return false;
-//        }
-//
-//        final String xmlStr = writer.toString();
-//
-//        /*
-//         * Convert the XML into HTTP POST data
-//         */
-//        String postDataStr;
-//        try {
-//            postDataStr = "xml=" + URLEncoder.encode(xmlStr, "UTF-8");
-//        } catch (UnsupportedEncodingException e) {
-//            return false;
-//        }
-//
-//        /*
-//         * Send the data to the server
-//         */
-//        byte[] postData = postDataStr.getBytes();
-//
-//        InputStream stream = null;
-//        try {
-//            URL url = new URL(SAVE_URL);
-//
-//            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-//
-//            conn.setDoOutput(true);
-//            conn.setRequestMethod("POST");
-//            conn.setRequestProperty("Content-Length", Integer.toString(postData.length));
-//            conn.setUseCaches(false);
-//
-//            OutputStream out = conn.getOutputStream();
-//            out.write(postData);
-//            out.close();
-//
-//            int responseCode = conn.getResponseCode();
-//            if(responseCode != HttpURLConnection.HTTP_OK) {
-//                return false;
-//            }
-//
-////            BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-////            String line;
-////            while ((line = reader.readLine()) != null) {
-////                Log.i("hatter", line);
-////            }
-//
-//            stream = conn.getInputStream();
-//
-//            /**
-//             * Create an XML parser for the result
-//             */
-//            try {
-//                XmlPullParser xmlR = Xml.newPullParser();
-//                xmlR.setInput(stream, UTF8);
-//
-//                xmlR.nextTag();      // Advance to first tag
-//                xmlR.require(XmlPullParser.START_TAG, null, "hatter");
-//
-//                String status = xmlR.getAttributeValue(null, "status");
-//                if(status.equals("no")) {
-//                    return false;
-//                }
-//
-//                // We are done
-//            } catch(XmlPullParserException ex) {
-//                return false;
-//            } catch(IOException ex) {
-//                return false;
-//            }
-//
-//        } catch (MalformedURLException e) {
-//            return false;
-//        } catch (IOException ex) {
-//            return false;
-//        } finally {
-//            if(stream != null) {
-//                try {
-//                    stream.close();
-//                } catch(IOException ex) {
-//                    // Fail silently
-//                }
-//            }
-//        }
-//        return true;
-//    }
-//
-//    public boolean deleteFromCloud(final String id) {
-//        // Create a get query
-//        String query = DELETE_URL + "?user=" + USER + "&magic=" + MAGIC + "&pw=" + PASSWORD + "&id=" + id;
-//
-//        InputStream stream = null;
-//
-//        try {
-//            URL url = new URL(query);
-//
-//            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-//            int responseCode = conn.getResponseCode();
-//            if(responseCode != HttpURLConnection.HTTP_OK) {
-//                return false;
-//            }
-//
-//            stream = conn.getInputStream();
-//
-//            /**
-//             * Create an XML parser for the result
-//             */
-//            try {
-//                XmlPullParser xmlR = Xml.newPullParser();
-//                xmlR.setInput(stream, UTF8);
-//
-//                xmlR.nextTag();      // Advance to first tag
-//                xmlR.require(XmlPullParser.START_TAG, null, "hatter");
-//
-//                String status = xmlR.getAttributeValue(null, "status");
-//                if(status.equals("no")) {
-//                    return false;
-//                }
-//
-//                // We are done
-//            } catch(XmlPullParserException ex) {
-//                return false;
-//            } catch(IOException ex) {
-//                return false;
-//            }
-//
-//
-//            return true;
-//
-//        } catch (MalformedURLException e) {
-//            // Should never happen
-//            return false;
-//        } catch (IOException ex) {
-//            return false;
-//        }
-//    }
-//
-//    public static void logStream(InputStream stream) {
-//        BufferedReader reader = new BufferedReader(
-//                new InputStreamReader(stream));
-//
-//        Log.e("476", "logStream: If you leave this in, code after will not work!");
-//        try {
-//            String line;
-//            while ((line = reader.readLine()) != null) {
-//                Log.e("476", line);
-//            }
-//        } catch (IOException ex) {
-//            return;
-//        }
-//    }
 }
